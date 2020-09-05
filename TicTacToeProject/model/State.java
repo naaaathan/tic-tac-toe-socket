330732bc938997101725c96c71fe2551d7920fe5
Nathan Augusto <nathan.caugusto@gmail.com>
2020-09-05 17:31:52 -0300

    Project TicTacToe - Sockets, Threads and JPA

---
+++ TicTacToeProject/model/State.java
@@ -0,0 +1,45 @@
+package model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name = "state")
+public class State {
+
+    @Id
+    private String code;
+
+    @Column
+    private int position;
+
+    @Column
+    private String isMarked;
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public void setPosition(int position) {
+        this.position = position;
+    }
+
+    public void setIsMarked(String isMarked) {
+        this.isMarked = isMarked;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public String getIsMarked() {
+        return isMarked;
+    }
+}
